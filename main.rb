f2e9bf09d4d9b6be3e4e3d79e5ef537d2db5afad
Vasyl <wkichira@gmail.com>
2020-05-19 13:05:17 +0300

    add main.rb(class Converter) and start.rb(main.file)

--- main.rb
+++ main.rb
@@ -1,1 +1,33 @@
-require 'converter'
+require 'net/http'
+require 'json'
+
+class Converter
+  def initialize(total, from="USD", to = "UAH")
+    @total = total
+    @from = from
+    @to = to
+  end
+
+  def convert!
+    dict = get_value_of_currency_exchange
+    rate = extract_exchange_rate(dict)
+    @total * rate
+  end
+
+  private
+
+  def api_url
+    api_id = "1d1a713fa9134ef48f82bd595b866f87"
+    "https://openexchangerates.org/api/latest.json?app_id=#{api_id}&base=#{@from}&symbols=#{@to}"
+  end
+
+  def get_value_of_currency_exchange
+    url = URI(api_url)
+    Net::HTTP.get(url)
+  end
+
+  def extract_exchange_rate(dict)
+    JSON.parse(dict).dig("rates", @to)
+  end
+end
+
--- start.rb
+++ start.rb
@@ -1,1 +1,14 @@
+require_relative  'main'
 
+start = true
+
+while start
+  print 'input your dollar amount -  '
+  amount = gets.to_i
+  puts Converter.new(amount).convert!
+  print 'Do you want to continue? input "y" if yes or "n" if no -  '
+  tmp = gets.chomp
+  if tmp == "n"
+    start = false
+  end
+end
